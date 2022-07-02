3fcfa61c53492f36c3b7d53734628aab4a2ac9ed
Siam300 <siamkhan8523@gmail.com>
2022-07-02 00:37:11 -0700

    Added Cat and Dog sounds

--- Cat and Dog/ViewController.swift
+++ Cat and Dog/ViewController.swift
@@ -6,25 +6,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer!
+    
+    
     @IBOutlet weak var photoDsplay: UIImageView!
     
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
     @IBAction func caButton(_ sender: UIButton) {
         photoDsplay.image = #imageLiteral(resourceName: "Cat")
+        //playCat(Sounds: "Cat")
+        playSound(name: "Cat")
+    }
+    
+    @IBAction func dogButton(_ sender: UIButton) {
+        photoDsplay.image = #imageLiteral(resourceName: "Dog")
+        //playDog(Sounds: "Dog")
+        playSound(name: "Dog")
         
     }
     
-    @IBAction func dogButton(_ sender: Any) {
-        photoDsplay.image = #imageLiteral(resourceName: "Dog")
     
+//    func playCat(Sounds: String) {
+//        let url = Bundle.main.url(forResource: "Cat", withExtension: "wav")
+//        player = try! AVAudioPlayer(contentsOf: url!)
+//        player.play()
+//        if  let catSound = Bundle.main.path(forResource: "Cat", ofType: "wav") {
+//            player = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: catSound))
+//            player?.play()
+//        }else {
+//         print("Cat File is missing")
+//        }
+//
+//
+//    }
+    
+//    func playDog(Sounds: String) {
+//        let url = Bundle.main.url(forResource: "Dog", withExtension: "wav")
+//        player = try! AVAudioPlayer(contentsOf: url!)
+//        player.play()
+        
+//        if  let dogSound = Bundle.main.path(forResource: "Dog", ofType: "wav") {
+//            player = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: dogSound))
+//            player?.play()
+//        }else {
+//         print("Dog File is missing")
+//        }
+//    }
+    
+    func playSound(name: String){
+        if let soundFile = Bundle.main.path(forResource: name, ofType: "wav"){
+            player = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile))
+            player?.play()
+        }
+        else{
+            print("\(name) File Missing!")
+        }
     }
-    
 }
 
